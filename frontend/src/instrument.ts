bca04031c1d33931c141026f462e3d6f244dda9b
Jayden Pham <jaydenphamofficial@gmail.com>
2026-06-14 17:52:02 +1200

    fix: update Sentry DSN to the correct value

--- frontend/src/instrument.ts
+++ frontend/src/instrument.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/react';
 
 Sentry.init({
-  dsn: 'https://fa98a6ea67ad11f1b80a929846016904@o4511562008952832.ingest.us.sentry.io/4511562016620544',
+  dsn: 'https://f2dafa5ee71be42cc69265c79e67fb83@o4511562008952832.ingest.us.sentry.io/4511562016620544',
   // To disable sending user data, uncomment the line below. For more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
   // dataCollection: { userInfo: false },
